7c68caf277a5ea6a0c0a3685b59c1f97d5c65b32
retrobot2000 <retrobot2000@gmail.com>
2022-05-05 19:20:27 -0600

    Non functioning welcome window - added pseudocode

--- Objection!!!/Objection___.py
+++ Objection!!!/Objection___.py
@@ -1,30 +1,63 @@
-#import the appropriate libraries
+# Import the appropriate libraries
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import *
 from tkinter import PhotoImage
-#test me
-#create our window
+
+# How this works is when you see the prosecutor pop up, click him and an objection will appear next to him
+# If you see an objection next to Phoenix, click on the objection and one will pop up on the prosecutors side
+# Game over if you touch any empty space, an objection on the prosecutors side, if you don't hit the keys on time, or Phoenix
+# Game win after 10 successful counters
+
+###########
+# Tkinter #
+###########
+
+# Create welcome window
+welcome = tk.Tk()
+# Set title
+welcome.title("Ace Attorney: Menu")
+# Set window size
+welcome.geometry("300x300")
+
+# Create main game window
 win = tk.Tk()
-#set its title
+# Set title
 win.title("Ace Attorney")
-#set the window size
+# Set window size
 win.geometry("1600x300")
 
-#create the 3 variables to hold the images
+##########
+# Images #
+##########
 PW = PhotoImage(file="phoenix-wright.png")
 ME = PhotoImage(file="miles-edgeworth.png")
 OBJ = PhotoImage(file="objection.png")
 E = PhotoImage(file="Empty.png")
 
+# Columns for Phoenix
 buttonname = tk.Button(image= PW)
 buttonname.grid(row=0,column=1)
 
+# Objection Columns
 buttonname = tk.Button(image= E)
 buttonname.grid(row=0,column=2)
+# Swap for column 2
 buttonname = tk.Button(image= E)
 buttonname.grid(row=0,column=3)
+# Swap for column 3
 
-buttonname = tk.Button(image= ME)
+
+# Edgeworth Columns
+buttonname = tk.Button(image= E)
 buttonname.grid(row=0,column=4)
+
+##############
+# Win Screen #
+##############
+
+###############
+# Lose Screen #
+###############
+
 tk.mainloop()
