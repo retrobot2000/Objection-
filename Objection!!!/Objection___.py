a1557464c165af60075969974fcdfb0a475b98b4
retrobot2000 <retrobot2000@gmail.com>
2022-05-11 16:27:05 -0600

    Added logo - Game changes

--- Objection!!!/Objection___.py
+++ Objection!!!/Objection___.py
@@ -9,73 +9,100 @@ from tkinter import PhotoImage
 # Game over if you touch any empty space, an objection on the prosecutors side, if you don't hit the keys on time, or Phoenix
 # Game win after 10 successful counters
 
-###########
-# Windows #
-###########
-def wel_win():
-	""" Create welcome window """
-	global win
-	window.destroy()
-	welcome = tk.Tk()
-	# Set title
-	welcome.title("Ace Attorney: Menu")
-	# Set window size
-	welcome.geometry("300x300")
-
-def set_win():
-	""" Create settings window """
-	global win
-	window.destroy()
-	settings = tk.Tk()
-	# Set title
-	settings.title("Ace Attorney: Settings")
-	# Set window size
-	settings.geometry("300x300")
-
-def gme_win():
-	""" Create Game window """
-	global win
-	window.destroy()
-	game = tk.Tk()
-	# Set title
-	game.title("Ace Attorney: Menu")
-	# Set window size
-	game.geometry("900x300")
-
-
-	# Columns for Phoenix
-	buttonname = tk.Button(image= PW)
-	buttonname.grid(row=0,column=1)
-
-	# Objection Columns
-	buttonname = tk.Button(image= E)
-	buttonname.grid(row=0,column=2)
-	# Swap for column 2
-	buttonname = tk.Button(image= E)
-	buttonname.grid(row=0,column=3)
-	# Swap for column 3
-
-
-	# Edgeworth Columns
-	buttonname = tk.Button(image= E)
-	buttonname.grid(row=0,column=4)
-
 ##########
 # Images #
 ##########
 def load_images():
 	""" Loads all images because it can """
+	global PW
+	global ME
+	global OBJ
+	global E
+	global L
 	PW = PhotoImage(file="phoenix-wright.png")
 	ME = PhotoImage(file="miles-edgeworth.png")
 	OBJ = PhotoImage(file="objection.png")
 	E = PhotoImage(file="Empty.png")
+	L = PhotoImage(file="Logo.png")
 
-##############
-# Win Screen #
-##############
+###########
+# Windows #
+###########
 
-###############
-# Lose Screen #
-###############
+def wel_win():
+	""" Create welcome window """
+	global window
+	window = tk.Tk()
+	# Set title
+	window.title("Ace Attorney: Menu")
+	# Set window size
+	window.geometry("1600x600")
+
+	# Load images for intro graphic
+	load_images()
+
+	#create a label and put an image on
+	lbl = tk.Label(window,image=L)
+	lbl.grid(row=0,column=0)
+	
+
+
+
+def set_win():
+	""" Create settings window """
+	global window
+	window.destroy()
+	window = tk.Tk()
+	# Set title
+	window.title("Ace Attorney: Settings")
+	# Set window size
+	window.geometry("1600x600")
+	
+	# Load images for intro graphic
+	load_images()
+
+def gme_win():
+	""" Create Game window """
+	global window
+
+	window = tk.Tk()
+	# Set title
+	window.title("Ace Attorney: OBJECTION!!!")
+	# Set window size
+	window.geometry("1600x600")
+
+	# Load images for intro graphic
+	load_images()
+
+	# Empty 1st row
+	lbl = tk.Label(window,image=E)
+	lbl.grid(row=0,column=0)
+	lbl = tk.Label(window,image=E)
+	lbl.grid(row=0,column=1)
+	lbl = tk.Label(window,image=E)
+	lbl.grid(row=0,column=2)
+	lbl = tk.Label(window,image=E)
+	lbl.grid(row=0,column=3)
+
+	# Columns for Phoenix
+	buttonname = tk.Button(image= PW)
+	buttonname.grid(row=1,column=0)
+
+	# Objection Columns
+	buttonname = tk.Button(image= E)
+	buttonname.grid(row=1,column=1)
+	# Swap for column 2
+	buttonname = tk.Button(image= E)
+	buttonname.grid(row=1,column=2)
+	# Swap for column 3
+
+
+	# Edgeworth Columns
+	buttonname = tk.Button(image= E)
+	buttonname.grid(row=1,column=3)
+
+
+
+wel_win()
 
 tk.mainloop()
