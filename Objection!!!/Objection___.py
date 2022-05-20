811acd318709f5ed16ded6eba6585cd53711f429
retrobot2000 <104224120+retrobot2000@users.noreply.github.com>
2022-05-20 12:01:45 -0600

    Big change to everything

--- Objection!!!/Objection___.py
+++ Objection!!!/Objection___.py
@@ -44,6 +44,25 @@ def ext_click(argument):
 # Windows #
 ###########
 
+#function to call after the timer expires while there is no snake visible
+def displaySnake(lbl):
+    #swap out the image of lbl
+    print('Event Triggered')
+    lbl['image'] = ME
+    lbl.after(1000,displayNoSnake,lbl)
+
+#function to call after the timer expires while there is a snake visible
+def displayNoSnake(lbl):
+    lbl['image'] = PW
+    lbl.after(2000,displaySnake,lbl)
+
+def labelClick(event):
+	print(event)
+	widget = event.widget
+	if widget['image'] == PW.name:
+		#no snake image was clicked
+		print('The image with no snake was clicked')
+
 def wel_win():
 	""" Create welcome window """
 	global window
@@ -81,8 +100,6 @@ def set_win():
 	# Set window size
 	window.geometry("1600x600")
 	
-	# Load images for intro graphic
-	load_images()
 
 def gme_win():
 	""" Create Game window """
@@ -94,37 +111,38 @@ def gme_win():
 	# Set window size
 	window.geometry("1600x600")
 
-	# Load images for intro graphic
+	# Load images for characters
 	load_images()
 
-	# Empty 1st row
-	lbl = tk.Label(window,image=E)
-	lbl.grid(row=0,column=0)
-	lbl = tk.Label(window,image=E)
-	lbl.grid(row=0,column=1)
-	lbl = tk.Label(window,image=E)
-	lbl.grid(row=0,column=2)
-	lbl = tk.Label(window,image=E)
-	lbl.grid(row=0,column=3)
+	for y in range(2):
+		for x in range(1):
+		# Empty 1st row
+			lbl = tk.Label(window,image=PW)
+			lbl.grid(row=y,column=x)
+			lbl.after(2000,displaySnake,lbl)
+			lbl.bind("<ButtonPress-1>",labelClick)
 
-	# Columns for Phoenix
-	buttonname = tk.Button(image= PW)
-	buttonname.grid(row=1,column=0)
+	## Columns for Phoenix
+	#buttonname = tk.Button(image= PW)
+	#buttonname.grid(row=1,column=0)
 
-	# Objection Columns
-	buttonname = tk.Button(image= E)
-	buttonname.grid(row=1,column=1)
-	# Swap for column 1
-
-
-	buttonname = tk.Button(image= E)
-	buttonname.grid(row=1,column=2)
-	# Swap for column 2
+	## Objection Columns
+	## Objection popup - random times - when clicked, makes column 2 pop up
+	#clm1 = tk.Button(image= E)
+	#clm1.grid(row=1,column=1)
+	## Swap for column 1
 
 
-	# Edgeworth Columns
-	buttonname = tk.Button(image= E)
-	buttonname.grid(row=1,column=3)
+	## Do not click this when objection pops up
+	#clm2 = tk.Button(image= E)
+	#clm2.grid(row=1,column=2)
+	## Swap for column 2
+
+
+	## Edgeworth Columns - pop up at random times
+	## Edgeworth popup - when clicked, column 2 pops up
+	#clm3 = tk.Button(image= E)
+	#clm3.grid(row=1,column=3)
 
 
 
